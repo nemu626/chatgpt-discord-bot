f44154e1b061cded00affd4ebe1ddb02bc6cf75f
nemu626 <yamada0626@gmail.com>
2023-01-25 00:04:51 +0900

    first commit:

---
+++ bot.js
@@ -0,0 +1,59 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const { Configuration, OpenAIApi } = require("openai");
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+    ]
+}
+);
+const arg = require('arg');
+
+const args = arg({
+    '--token': String,
+    '--apiKey': String,
+});
+
+const token = args['--token'];
+const apiKey = args['--apiKey'];
+
+const configuration = new Configuration({
+    apiKey: apiKey
+});
+const openAIApi = new OpenAIApi(configuration);
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('messageCreate', msg => {
+    if (msg.author.bot) return;
+    if (msg.content.startsWith("!")) {
+        const command = msg.content.substring(1);
+        if (command === "help") {
+            msg.channel.send("**Available commands:**\n" +
+                "!help - shows this message\n" +
+                "any text - send any text to receive GPT-3 generated response");
+        }
+    } else if (msg.mentions.has(client.user)) {
+        const question = msg.content.replace(/<@(.+)>/, '');
+        console.log('* QUESTION:', question);
+        msg.channel.sendTyping();
+        openAIApi.createCompletion({
+            prompt: question,
+            model: "text-davinci-003",
+            max_tokens: 256,
+        }).then(({ data, status }) => {
+            if (data && status === 200) {
+                console.log(data)
+                const answer = data.choices[0].text || '';
+                console.log('* ANSWER   : ', answer);
+                msg.reply(answer)
+            }
+        });
+    }
+});
+
+client.login(token);
