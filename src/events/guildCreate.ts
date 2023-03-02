7a1073b2c99eba1ddcef1bf188cbf885fdf2430c
nemu626 <yamada0626@gmail.com>
2023-03-02 20:04:40 +0900

    add greeting message when join

--- src/events/guildCreate.ts
+++ src/events/guildCreate.ts
@@ -2,6 +2,8 @@ import { ArgsOf, Client } from "discordx"
 
 import { Discord, On } from "@decorators"
 import { syncGuild } from "@utils/functions"
+import { ChannelType, GuildBasedChannel, PermissionsBitField, TextChannel } from "discord.js"
+import { env } from "process"
 
 @Discord()
 export default class GuildCreateEvent {
@@ -13,5 +15,12 @@ export default class GuildCreateEvent {
     ) {
 
         await syncGuild(newGuild.id, client)
+        
+        const channel: GuildBasedChannel | undefined = newGuild.channels.cache.find(channel =>
+            channel.type === ChannelType.GuildText &&
+            !!newGuild.members.me &&
+            channel.permissionsFor(newGuild.members.me).has('SendMessages'))
+        if (channel?.isTextBased() && process.env.GREETING_MESSAGE)
+            channel.send(process.env.GREETING_MESSAGE)
     }
 }
