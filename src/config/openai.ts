0d71f3b363f96d10de58f6f6415c4cbfa3e65db7
nemu626 <yamada0626@gmail.com>
2023-03-05 00:28:00 +0900

    🚧 add constants and function for openAI(

---
+++ src/config/openai.ts
@@ -0,0 +1,5 @@
+export const OPENAI_CHAT_MODEL = 'gpt-3.5-turbo';
+export const TOKEN_MAX_COUNT = 4096; //see openai API documents.
+
+export const DEFAULT_MAX_PROMPT_TOKEN = 2000;
+export const DEFAULT_SYSTEM_MESSAGE = 'You are chatbot assistant.'; 
