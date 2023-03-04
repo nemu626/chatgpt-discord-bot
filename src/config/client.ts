a7f654ed6d31f4d21699903969732aeb66eece90
nemu626 <yamada0626@gmail.com>
2023-03-05 01:54:21 +0900

    🔧 change lint space to tab

--- src/config/client.ts
+++ src/config/client.ts
@@ -1,12 +1,12 @@
 import { GatewayIntentBits } from 'discord.js';
 
 export const DefaultClientIntents: GatewayIntentBits[] = [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMembers,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.GuildMessageReactions,
-    GatewayIntentBits.GuildVoiceStates,
-    GatewayIntentBits.GuildPresences,
-    GatewayIntentBits.DirectMessages,
-    GatewayIntentBits.MessageContent
+	GatewayIntentBits.Guilds,
+	GatewayIntentBits.GuildMembers,
+	GatewayIntentBits.GuildMessages,
+	GatewayIntentBits.GuildMessageReactions,
+	GatewayIntentBits.GuildVoiceStates,
+	GatewayIntentBits.GuildPresences,
+	GatewayIntentBits.DirectMessages,
+	GatewayIntentBits.MessageContent
 ];
--- src/index.ts
+++ src/index.ts
@@ -11,42 +11,42 @@ const apiKey: string = process.env.OPENAI_APIKEY || '';
 const openAIApi = new OpenAIApi(new Configuration({ apiKey: apiKey }));
 
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user?.tag}!`);
+	console.log(`Logged in as ${client.user?.tag}!`);
 });
 
 client.on('guildCreate', (guild: Guild) => {
-    const mainChannel: GuildBasedChannel | undefined = guild.channels.cache.find(ch =>
-        ch.type === ChannelType.GuildText &&
+	const mainChannel: GuildBasedChannel | undefined = guild.channels.cache.find(ch =>
+		ch.type === ChannelType.GuildText &&
         !!guild.members.me &&
         ch.permissionsFor(guild.members.me).has('SendMessages'));
-    if (mainChannel?.isTextBased() && process.env.GREETING_MESSAGE) {
-        mainChannel.send(process.env.GREETING_MESSAGE);
-    }
+	if (mainChannel?.isTextBased() && process.env.GREETING_MESSAGE) {
+		mainChannel.send(process.env.GREETING_MESSAGE);
+	}
 });
 
 client.on('messageCreate', (msg: Message) => {
-    if (msg.author.bot ||
+	if (msg.author.bot ||
         !msg.channel ||
         msg.channel instanceof (StageChannel) ||
         !client.user ||
         !msg.mentions.has(client.user)) return;
 
-    const question = msg.cleanContent;
-    console.log(`[Question] ${msg.author?.username} :: ${question}`);
+	const question = msg.cleanContent;
+	console.log(`[Question] ${msg.author?.username} :: ${question}`);
 
-    msg.channel.sendTyping();
+	msg.channel.sendTyping();
 
-    openAIApi.createChatCompletion({
-        model: OPENAI_CHAT_MODEL,
-        temperature: DEFAULT_TEMPERATURE,
-        messages: [{ 'role': 'user', 'content': question }]
-    }).then(({ status, data }) => {
-        if (status !== 200 || !data) return;
+	openAIApi.createChatCompletion({
+		model: OPENAI_CHAT_MODEL,
+		temperature: DEFAULT_TEMPERATURE,
+		messages: [{ 'role': 'user', 'content': question }]
+	}).then(({ status, data }) => {
+		if (status !== 200 || !data) return;
 
-        const answer: string = data.choices?.[0].message?.content || '';
-        console.log(`[Answer] chatGPT :: ${answer}`);
+		const answer: string = data.choices?.[0].message?.content || '';
+		console.log(`[Answer] chatGPT :: ${answer}`);
 
-    });
+	});
 
 });
 
