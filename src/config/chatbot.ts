4c5eb93db9dcca40fd44641d30d7b7bcecbc4693
nemu626 <yamada0626@gmail.com>
2023-03-05 11:52:19 +0900

    🔧 change config value

--- src/config/chatbot.ts
+++ src/config/chatbot.ts
@@ -1,7 +1,8 @@
+import { TOKEN_MAX_COUNT } from './openai';
 import { ChatBot } from '../types';
 
 export const DEFAULT_TEMPERATURE = 0.5;
-export const DEFAULT_MAX_PROMPT_TOKEN = 2000;
+export const DEFAULT_MAX_PROMPT_TOKEN = TOKEN_MAX_COUNT / 2;
 export const DEFAULT_SYSTEM_MESSAGE = 'You are chatbot assistant.';
 export const DEFAULT_GREETING_MESSAGE = 'I am a chatbot using chatGPT. Mention me and I will answer you.';
 
